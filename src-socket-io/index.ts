ade8afcb45c3d1ef7489c4c6ae090dffb3fa6aa8
fancyjimmy <jimmyfan04@gmail.com>
2023-04-17 20:59:11 +0200

    testing netlify

--- src-socket-io/index.ts
+++ src-socket-io/index.ts
@@ -16,6 +16,7 @@ const serverHandlers = [
 export default function injectSocketIO(server: HttpServer) {
     const io = new Server(server, {
         cors: {
+            origin: "https://benevolent-duckanoo-c52100.netlify.app/",
             methods: ['GET', 'POST'],
         }
     });
