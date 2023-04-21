09ded343eca4d2833a455eb458c6b849b91c813f
fancyjimmy <jimmyfan04@gmail.com>
2023-04-21 19:37:10 +0200

    ChooseOldestRolePolicy.ts tested

--- src-socket-io/lib/handler/lobby/policy/role/ChooseOldestRolePolicy.ts
+++ src-socket-io/lib/handler/lobby/policy/role/ChooseOldestRolePolicy.ts
@@ -1,8 +1,12 @@
-import type {Player, PlayerInfo} from "../../types";
-import type RolePolicy from "./RolePolicy";
+import type {Player, PlayerInfo} from '../../types';
+import {LobbyRole} from '../../types';
+import type RolePolicy from './RolePolicy';
 
 export default class ChooseOldestRolePolicy implements RolePolicy {
-    nextHost(players: Player[], leavingPlayer: PlayerInfo): Player {
+    nextHost(players: Player[], leavingPlayer: PlayerInfo): Player | null {
+        if (leavingPlayer.role !== LobbyRole.HOST) return null;
+        if (players.length === 0) return null;
+
         let nextHost = players[0];
         for (let i = 1; i < players.length; i++) {
             const player = players[i];
@@ -12,5 +16,4 @@ export default class ChooseOldestRolePolicy implements RolePolicy {
         }
         return nextHost;
     }
-
 }
